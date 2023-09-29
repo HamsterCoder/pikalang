9c928cf5c078ced4148a82f86046588a763d0aeb
HamsterCoder <sirinity13@gmail.com>
2023-09-29 12:44:39 +0200

    Do not use Google Analytics in development

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        ViteEjsPlugin((viteConfig) => ({
+            // viteConfig is the current Vite resolved config
+            env: viteConfig.env,
+        })),
+        react(),
+    ],
     resolve: {
         alias: [
             {
